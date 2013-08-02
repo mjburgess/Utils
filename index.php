ed0862950514821e2da11b97130deea50b154787
Michael John Burgess <michael@mjburgess.co.uk>
2013-08-02 09:41:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ div.body {
 	$inbox = imap_open($hostname,$username,$password) or die('Cannot connect to Gmail: ' . imap_last_error());
 	
 	/* grab emails */
-	$emails = imap_search($inbox,'ALL');
+	$emails = imap_search($inbox,'UNSEEN');
 	
 	/* if emails are returned, cycle through each... */
 	if($emails) {
