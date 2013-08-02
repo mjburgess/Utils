fc9e493d4e1253be3f0a39c31d582a57c5592d83
Michael John Burgess <michael@mjburgess.co.uk>
2013-08-02 12:09:56 +0100

    Create email.php

---
+++ email.php
@@ -0,0 +1,33 @@
+<?php
+
+function gmail_connect($username, $password) {
+  $hostname = '{imap.gmail.com:993/imap/ssl}INBOX';
+
+	return imap_open($hostname, $username, $password) or die('Cannot connect to Gmail: ' . imap_last_error());
+}
+
+function gmail_today($inbox) {
+	$e =  imap_search($inbox, 'ON "' . date ('d M Y') . '"');
+	rsort($e);
+	return $e;
+}
+
+function gmail_read($inbox, $email_number) {
+	$overview = imap_fetch_overview($inbox,$email_number,0);
+
+	$body = imap_fetchbody($inbox, $email_number, 1.1);
+
+	if (!$body) {
+	    $body = imap_fetchbody($inbox, $email_number, 1);
+	}
+			
+	if (!$body) {
+	    $body = imap_fetchbody($inbox, $email_number, 2);
+	}
+			
+	$body = trim(substr(quoted_printable_decode($body), 0, 1000));
+
+	$overview[0]->body = $body;
+
+	return $overview[0];
+}
