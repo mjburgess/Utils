b17da9b5c436076ab7480e23bae69b2444169c6c
Michael John Burgess <michael@mjburgess.co.uk>
2013-08-02 13:55:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,32 +14,26 @@
   });
   </script>
 </head>
-
 <body>
 <?php if(empty($_POST['username'])): ?>
-
-<form action="" method="post">
-	<div>U: <input type="text" name="username" id="username" /></div>
-	<div>P: <input type="password" name="password" id="password" /></div>
-	<input type="submit" name="submit" value="submit" />
-</form>
-
+	<form action="" method="post">
+		<div>U: <input type="text" name="username" id="username" /></div>
+		<div>P: <input type="password" name="password" id="password" /></div>
+		<input type="submit" name="submit" value="submit" />
+	</form>
 <?php else:  ?>
-<div id="accordion"> 
-<?php $emails = gmail_today($inbox = gmail_connect($_POST['username'], $_POST['password'])); ?>
-
-<?php if(!$emails): ?> 
-	<h1>No Emails Today!</h3> 
-<?php endif; ?>
-
-<?php foreach($emails as $email_number): $email = gmail_read($inbox, $email_number); ?>
-<h3><?php echo $email->subject; ?> <em>from, <?php echo $email->from; ?>, on <?php echo $email->date; ?></em></h3>
-<div><p>
-<?php echo $email->body; ?></p></div>
-<?php endforeach; ?>
+	<div id="accordion"> 
+		<?php $emails = gmail_today($inbox = gmail_connect($_POST['username'], $_POST['password'])); ?>
+		
+		<?php if(!$emails): ?> 
+			<h1>No Emails Today!</h3> 
+		<?php endif; ?>
+		
+		<?php foreach($emails as $email_number): $email = gmail_read($inbox, $email_number); ?>
+			<h3><?php echo $email->subject; ?> <em>from, <?php echo $email->from; ?>, on <?php echo $email->date; ?></em></h3>
+			<div><p><?php echo $email->body; ?></p></div>
+		<?php endforeach; ?>
 	</div>
 <?php endif; ?>
-
-
 </body>
 </html>
